db28a02c558a40ccd7f6d4851b3a8e71b0398785
Marcos <marcos.santos@codenapp.com>
2022-01-13 15:59:59 -0300

    final

--- imc/android/app/src/main/kotlin/com/example/imc/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.imc
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- imc/lib/binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:imc/controller.dart';
-
-class HomeBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<HomeController>(() => HomeController());
-  }
-}
